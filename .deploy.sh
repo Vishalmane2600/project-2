3273a521261ac1bc61c2c3d7f7a7bd3d82f94d5a
Vishal Mane <95902011+Vishalmane2600@users.noreply.github.com>
2026-09-21 00:17:41 +0530

    Add files via upload

---
+++ .deploy.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+set -e
+
+echo "=================================="
+echo "Updating source code.."
+echo "=================================="
+sudo git pull
+
+echo "=================================="
+echo "Installing dependencies..."
+echo "=================================="
+sudo npm install
+
+echo "=================================="
+echo "Building React application..."
+echo "=================================="
+sudo npm run build
+
+echo "=================================="
+echo "Deploying build..."
+echo "=================================="
+sudo cp -r dist/* /TripTour
+
+
+sudo nginx -t
+
+echo "=================================="
+echo "Reloading Nginx..."
+echo "=================================="
+sudo systemctl reload nginx
+
+echo ""
+echo "=================================="
+echo "✅ Deployment completed successfully!"
+echo "=================================="
+
+echo ""
+echo "Application URL:"
